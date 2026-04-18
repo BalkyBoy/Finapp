8aef0e6231281c306f4616b841579ce791c802bf
BalkyBoy <bolugafar360@gmail.com>
2026-04-18 11:11:28 +0100

    chore: improve gitignore configuration for server and root level

--- server/internal/ledger/engine.go
+++ server/internal/ledger/engine.go
@@ -10,5 +10,15 @@ func New(db *sqlx.DB) Engine {
 }
 
 func (e *engine) CommitTransaction(ctx context.Context, ledger string, req TransactionRequest) (*Transaction, error) {
-	
+	if err := validateRequest(req); err != nil {
+		return nil, err
+	}
+
+	var committed *Transaction
+	err := e.withTx(ctx, func(tx *sqlx.Tx) error {
+		ledgerID, err := getLedgerID(ctx, tx, ledger)
+		if err != nil {
+			return err
+		}
+	})
 }
